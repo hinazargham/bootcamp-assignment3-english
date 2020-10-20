a126d9f31fb55d72014ce1d4a47cdf7b22edd8cd
Hina Zargham <hinaz0711@gmail.com>
2020-10-20 20:31:52 +0500

    update files
    updated app.js, index.css and index.js files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from "./hello.js";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1><b>BOOTCAMP2020</b></h1>
+          <h2>ASSIGNMENT no. {props.session - 1} ({props.class} Class) </h2>
+          <h3>REACT TUTORIAL by {props.teachers}</h3>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Hello myname="Hina Zargham" myhello="hello.js"></Hello>
+        <img src={logo} className="App-logo" alt="logo" width="250" />
+     
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <App teachers= "Sir ZIA KHAN and ZEESHAN HANIF" session= "4" class="ENGLISH"/>
+  </div>,
   document.getElementById('root')
 );
 
