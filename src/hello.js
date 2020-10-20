bc12caae17eb98137ce45d84a8706cdc5b291108
Hina Zargham <hinaz0711@gmail.com>
2020-10-20 20:32:12 +0500

    added files
    added hello.css and hello.js files

---
+++ src/hello.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./hello.css"
+
+function Hello({myhello, myname}) {
+
+    return (
+        
+        <div>
+        <p>HELLO from CHILD component ie. "{myhello}" file </p>
+        <br/>
+        <i>Submitted by {myname}</i>
+    </div>
+
+    )
+}
+
+
+export default Hello;
